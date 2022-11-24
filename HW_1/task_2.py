ef42545a191955d53b4404a289d64f45d27fd373
ikodzoev <loamaro@live.ru>
2022-11-25 00:14:04 +0500

    Новый вариант решения задачи №2

--- HW_1/task_2.py
+++ HW_1/task_2.py
@@ -3,8 +3,22 @@
 
 Вывод: единственное значение типа bool (True либо False)
 """
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            print(not (x or y or z) == (not x and not y and not z))
-            print(x, y, z)
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             print(not (x or y or z) == (not x and not y and not z))
+#             print(x, y, z)
+
+x = int(input('Введите значение X: '))
+y = int(input('Введите значение Y: '))
+z = int(input('Введите значение Z: '))
+
+
+def check_val():
+    if not (x or y or z) == (not x and not y and not z):
+        return True
+    else:
+        return False
+
+
+print(check_val())
