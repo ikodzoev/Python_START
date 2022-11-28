0344bf50675166bcac2ce754920559b3f854a0ea
ikodzoev <loamaro@live.ru>
2022-11-28 22:08:43 +0500

    Добавлена задача № 4

--- HW_2/task_4.py
+++ HW_2/task_4.py
@@ -6,3 +6,19 @@
 Ввод: значение типа <int>
 Вывод: значение типа <int>
 """
+from random import randint
+
+
+def getlist():
+    i_list = []
+    for i in range(n):
+        i_list.append(randint(-n, n))
+    return i_list
+
+
+n = 50
+numbers = getlist()
+print(numbers)
+x = open('indexes.txt', 'r')
+output = numbers[int(x.readline())] * numbers[int(x.readline(2))]
+print(output)
