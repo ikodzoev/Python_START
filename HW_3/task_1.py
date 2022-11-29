594df5a4b3f8dcc5b975ac07d090f4c717769660
ikodzoev <loamaro@live.ru>
2022-11-29 23:59:22 +0500

    Добавлена  задача № 1

--- HW_3/task_1.py
+++ HW_3/task_1.py
@@ -11,3 +11,12 @@
 [5, 1, 5, 2, 7, 11]
 14
 """
+
+getlist = []
+print('Введите числа через пробел: ')
+for element in input().split():
+    getlist.append(int(element))
+# print(getlist)
+sum_odd = sum(getlist[1::2])
+print(sum_odd)
+# print(type(sum_odd))
