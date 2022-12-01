08a738e89a3a80fadeaf327c7d4f064d9e414c2f
ikodzoev <loamaro@live.ru>
2022-12-01 15:44:16 +0500

    Добавлена задача № 3

--- HW_3/task_3.py
+++ HW_3/task_3.py
@@ -9,3 +9,27 @@
 [1.1, 1.2, 3.1, 5, 10.01]
 2.0
 """
+print('Введите вещественные числа через запятую: ')
+getlist = []
+for element in input().split(','):
+    getlist.append(float(element))
+
+
+# getlist = [1.1, 1.2, 3.1, 5, 10.01]
+
+
+def diff():
+    max_fract = 0
+    min_fract = 1
+    for i in getlist:
+        buf = round(i % 1, 2)
+        if buf > max_fract:
+            max_fract = buf
+        elif buf < min_fract:
+            min_fract = buf
+    res = (round((max_fract - min_fract), 2) * 10)
+    # print(f"max {max_fract}, min {min_fract}")
+    return res
+
+
+print(diff())
