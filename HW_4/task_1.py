00a002f302e5a3084c1c5c02a7813fee5cf55ff6
ikodzoev <loamaro@live.ru>
2022-12-03 23:10:55 +0500

    Add task #1 of lesson 4

--- HW_4/task_1.py
+++ HW_4/task_1.py
@@ -11,3 +11,13 @@
 38
 [2, 19]
 """
+number = int(input("Введите натуральное число: "))
+prev = 2
+getlist = []
+while prev <= number:
+    if number % prev == 0:
+        getlist.append(prev)
+        number //= prev
+    else:
+        prev += 1
+print(getlist)
