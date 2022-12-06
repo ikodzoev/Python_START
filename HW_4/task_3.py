e3e65d9a84664bbeda10a20272c93f64fcbd049e
ikodzoev <loamaro@live.ru>
2022-12-06 18:45:37 +0500

    Add task #3 of lesson 4

--- HW_4/task_3.py
+++ HW_4/task_3.py
@@ -10,3 +10,24 @@
 2
 2x^2 + 4x + 5 = 0
 """
+
+
+from random import randint
+
+k = int(input('Введите натуральное число k:'))
+
+max_num = 100
+
+ratio = [randint(0, max_num) for i in range(k)] + [randint(1, max_num)]
+polynom = '+'.join([f'{(j, "")[j == 1]}x^{i}' for i, j in enumerate(ratio) if j][::-1])
+
+polynom = polynom.replace('x^1+', 'x+')
+polynom = polynom.replace('x^0', '')
+polynom += ('', '1')[polynom[-1] == '+']
+polynom = (polynom, polynom[:-2])[polynom[-2:] == '^1']
+
+print(polynom, '= 0')
+
+with open("polynom_res.txt", 'w') as data:
+    data.write(str(polynom))
+    data.write(str(' = 0'))
