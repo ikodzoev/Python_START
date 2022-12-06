6b75b5307bcbd56bfb8fd5e47cc96d3fc3d85e51
ikodzoev <loamaro@live.ru>
2022-12-06 18:46:29 +0500

    Add task #4 of lesson 4

--- HW_4/task_4.py
+++ HW_4/task_4.py
@@ -11,3 +11,28 @@
 3x^2+2x+1=0
 9x^5+7x^4+7x^3+12x^2+8x+18=0
 """
+
+with open('polynom_res1.txt', 'r') as data:
+    polynom_res1 = data.read()
+with open('polynom_res2.txt', 'r') as data:
+    polynom_res2 = data.read()
+
+file_1 = open('polynom_res1.txt', 'r')
+file_2 = open('polynom_res2.txt', 'r')
+sum_poly = open('polynom_res_sum.txt', 'w')
+file1 = file_1.readline()
+file2 = file_2.readline()
+for i in range(len(file1)):
+    if file1[i - 1] != '^':
+        if file1[i].isnumeric():
+            sum_poly.write(str(int(file1[i]) + int(file2[i])))
+        else:
+            sum_poly.write(str(file1[i]))
+    else:
+        sum_poly.write(str(file1[i]))
+
+sum_poly = open('polynom_res_sum.txt', 'r')
+
+with open('polynom_res_sum.txt', 'r') as data:
+    polynom_res_sum = data.read()
+print(polynom_res_sum)
