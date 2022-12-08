b758425c3c706389e34d560d12701ced8a2220f6
ikodzoev <loamaro@live.ru>
2022-12-08 22:27:05 +0500

    Add task № 4 of lesson 5

--- HW_5/task_4.py
+++ HW_5/task_4.py
@@ -10,3 +10,41 @@
 ыыыыыррррр   аааааагггггггг
 5ы5р3 6а8г
 """
+text = input('Введите текст: ')
+
+
+def encoding(seq):
+    count = 1
+    res = []
+    for x, item in enumerate(seq):
+        if x == 0:
+            continue
+        elif item == seq[x - 1]:
+            count += 1
+        else:
+            res.append((seq[x - 1], count))
+            count = 1
+    res.append((seq[len(seq) - 1], count))
+    return res
+
+
+def decoding(seq):
+    res = []
+    for item in seq:
+        res.append(item[0] * item[1])
+    return ''.join(res)
+
+
+def cod_format(seq):
+    res = []
+    for item in seq:
+        if item[1] == 1:
+            res.append(item[0])
+        else:
+            res.append(str(item[1]) + item[0])
+    return ''.join(res)
+
+
+print('Изначальный текст: ' + text)
+print(f'Зашифрованный текст: {cod_format(encoding(text))}')
+print(f'Дешифрованный текст: {decoding(encoding(text))}')
