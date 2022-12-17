fae422dce4532bad22786de3a7ee571608a857d2
ikodzoev <loamaro@live.ru>
2022-12-17 22:56:43 +0500

    Edited for 'get_move'

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -5,7 +5,8 @@
 Вывод: значение типа <str>
 """
 s = input('Введите любой текст: ')
+get_move = 'абв'
 # # Удаляет все слова, в которых есть буквы «а», «б» и «в» (любая из букв):
-# print(' ' .join(list(filter(lambda str_: all(i not in str_.lower() for i in 'абв'), s.split()))))
+# print(' ' .join(list(filter(lambda str_: all(i not in str_.lower() for i in get_move), s.split()))))
 # # Удаляет все слова, в которых ОДНОВРЕМЕННО присутствуют буквы «а», «б» и «в» в любом порядке:
-print(' ' .join(list(filter(lambda str_: not all(i in str_.lower() for i in 'абв'), s.split()))))
+print(' ' .join(list(filter(lambda str_: not all(i in str_.lower() for i in get_move), s.split()))))
